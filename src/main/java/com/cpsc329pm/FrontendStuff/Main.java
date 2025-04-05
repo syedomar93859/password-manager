b86896876d9291f443d194327e18c6156397d42b
SyedOmar <syed.omar@ucalgary.ca>
2025-04-04 22:22:22 -0600

    Created Main filed where GUI should be created

---
+++ src/main/java/com/cpsc329pm/FrontendStuff/Main.java
@@ -0,0 +1,23 @@
+package com.cpsc329pm.FrontendStuff;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MainPage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Main Page");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
