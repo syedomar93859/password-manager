324a3d494f8499db873d478d521294f8018d3350
SyedOmar <syed.omar@ucalgary.ca>
2025-04-07 11:45:15 -0600

    Changed some files for it to hopefully work

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,13 +3,15 @@ module com.example.cpsc329pm {
     requires javafx.fxml;
     requires javafx.web;
     requires com.fasterxml.jackson.databind;
-
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
+
+    // Treat TilesFX as an automatic module, this might not work depending on version.
+//    requires eu.hansolo.tilesfx; // No module-info available for TilesFX, but Java might resolve it automatically.
+
     requires com.almasb.fxgl.all;
 
     opens com.cpsc329pm to javafx.fxml, com.fasterxml.jackson.databind;
