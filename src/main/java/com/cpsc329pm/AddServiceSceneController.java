192aa3f4459b5f98c58b66c9b8400283cb05148b
SyedOmar <syed.omar@ucalgary.ca>
2025-04-08 11:07:36 -0600

    Fixed issue of multiple windows popping up. Also Main Page is closed when clicking a button. Also added functionality for the back button Add Service Scene window.

--- src/main/java/com/cpsc329pm/AddServiceSceneController.java
+++ src/main/java/com/cpsc329pm/AddServiceSceneController.java
@@ -9,29 +9,29 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
 
-public class MainPageController {
+public class AddServiceSceneController {
 
     @FXML
-    private void handleAddService(ActionEvent event) {
+    private void handleBack(ActionEvent event) {
         try {
+            // Get current stage (Add New Service)
             Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
-            // Load AddServiceScene.fxml
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("AddServiceScene.fxml"));
+            // Load MainPage.fxml
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("MainPage.fxml"));
             Parent root = loader.load();
 
             // Create a new stage
             Stage stage = new Stage();
-            stage.setTitle("Add New Service");
+            stage.setTitle("Main Page");
             stage.setScene(new Scene(root));
             stage.show();
 
-            // Close the Main Page
+            // Close the current Add Service window
             currentStage.close();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
 }
