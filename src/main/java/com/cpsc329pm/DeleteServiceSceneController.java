1330a2d5339cba71d11363e09a523fd9afa89c0f
SyedOmar <syed.omar@ucalgary.ca>
2025-04-08 11:21:14 -0600

    Delete Service button in main page works. The Back button in the Delete Service Scene window also works.

---
+++ src/main/java/com/cpsc329pm/DeleteServiceSceneController.java
@@ -0,0 +1,38 @@
+package com.cpsc329pm;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class DeleteServiceSceneController {
+
+    @FXML
+    private void handleBack(ActionEvent event) {
+        try {
+            // Get current stage (Add New Service)
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            // Load MainPage.fxml
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("MainPage.fxml"));
+            Parent root = loader.load();
+
+            // Create a new stage
+            Stage stage = new Stage();
+            stage.setTitle("Main Page");
+            stage.setScene(new Scene(root));
+            stage.show();
+
+            // Close the current Add Service window
+            currentStage.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/com/cpsc329pm/MainPageController.java
+++ src/main/java/com/cpsc329pm/MainPageController.java
@@ -34,4 +34,26 @@ public class MainPageController {
         }
     }
 
+    @FXML
+    private void handleDeleteService(ActionEvent event) {
+        try {
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            // Load DeleteServiceScene.fxml
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("DeleteServiceScene.fxml"));
+            Parent root = loader.load();
+
+            // Create a new stage
+            Stage stage = new Stage();
+            stage.setTitle("Add New Service");
+            stage.setScene(new Scene(root));
+            stage.show();
+
+            // Close the Main Page
+            currentStage.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
