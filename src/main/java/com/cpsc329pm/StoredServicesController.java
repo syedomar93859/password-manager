cf34b52448917346ee21c83c2e5356add5fb1786
SyedOmar <syed.omar@ucalgary.ca>
2025-04-10 16:36:06 -0600

    View All Services page should work now

--- src/main/java/com/cpsc329pm/StoredServicesController.java
+++ src/main/java/com/cpsc329pm/StoredServicesController.java
@@ -5,6 +5,7 @@ import com.cpsc329pm.Data;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -13,33 +14,45 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import java.util.List;
 
 public class StoredServicesController {
+
+    private int num = 0;
+
     @FXML private TableView<Data> tableView;
     @FXML private TableColumn<Data, String> colService;
     @FXML private TableColumn<Data, String> colUsername;
     @FXML private TableColumn<Data, String> colPassword;
+    @FXML private TableColumn<Data, String> colNumber;
 
     private final DataStorage dataStorage = new DataStorage("master_login.json"); // Assuming you have this class
 
     @FXML
     public void initialize() {
+        // Set cell value factories for actual data columns
         colService.setCellValueFactory(new PropertyValueFactory<>("platform"));
         colUsername.setCellValueFactory(new PropertyValueFactory<>("username"));
         colPassword.setCellValueFactory(new PropertyValueFactory<>("password"));
 
+        // Set a custom cell value factory for row numbers
+        colNumber.setCellValueFactory(cellData -> {
+            int index = tableView.getItems().indexOf(cellData.getValue()) + 1;
+            return new javafx.beans.property.SimpleStringProperty(String.valueOf(index));
+        });
+
         loadDataIntoTable();
     }
 
-
+    @FXML
     private void loadDataIntoTable() {
+        num++;
         List<Data> dataList = dataStorage.getAllData();
 
         // DEBUG: Check if data is retrieved
-        System.out.println("Retrieved Data: " + dataList);
+//        System.out.println("Retrieved Data: " + dataList);
 
         ObservableList<Data> observableList = FXCollections.observableArrayList(dataList);
 
         // DEBUG: Check if ObservableList is not empty
-        System.out.println("ObservableList Size: " + observableList.size());
+//        System.out.println("ObservableList Size: " + observableList.size());
 
         tableView.setItems(observableList);
     }
